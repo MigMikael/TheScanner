1bfeb6153a16570101e920424e76d88386b72087
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2018-03-28 11:00:23 +0700

    change api endpoint

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -32,7 +32,7 @@ class ScanController extends Controller
 
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => 'http://pi.cp.su.ac.th/PI/QR/post_request.php',
+            CURLOPT_URL => 'http://netserv.cp.su.ac.th:8080/PI/QR/post_request.php',
             CURLOPT_USERAGENT => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
             CURLOPT_POST => 1,
             CURLOPT_POSTFIELDS => array(
